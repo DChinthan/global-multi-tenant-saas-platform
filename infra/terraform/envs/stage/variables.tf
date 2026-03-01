64e3967f46deda4c27adda66c152825b14ff1572
Chinthan Dinesh <chinthandp@gmail.com>
2026-02-28 21:50:48 -0500

    fix(iac): add missing stage env variable declarations

--- infra/terraform/envs/stage/variables.tf
+++ infra/terraform/envs/stage/variables.tf
@@ -1,9 +1,44 @@
-variable "env" {
-  type    = string
-  default = "stage"
-}
-
 variable "aws_region" {
   type    = string
   default = "us-east-1"
+}
+
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+# Cost-safety toggles
+variable "enable_nat" {
+  type    = bool
+  default = false
+}
+
+variable "enable_rds" {
+  type    = bool
+  default = false
+}
+
+variable "enable_opensearch" {
+  type    = bool
+  default = false
+}
+
+variable "enable_kinesis" {
+  type    = bool
+  default = false
+}
+
+# Optional sizing knobs (only if referenced in stage/main.tf)
+variable "rds_instance_class" {
+  type    = string
+  default = "db.t4g.micro"
+}
+
+variable "opensearch_instance_type" {
+  type    = string
+  default = "t3.small.search"
 }
