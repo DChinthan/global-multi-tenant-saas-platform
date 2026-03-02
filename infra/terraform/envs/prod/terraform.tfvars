b5c705fa56493c1e627b411f4926efe9347e0db9
Chinthan Dinesh <chinthandp@gmail.com>
2026-03-01 20:22:16 -0500

    chore(iac): terraform fmt

--- infra/terraform/envs/prod/terraform.tfvars
+++ infra/terraform/envs/prod/terraform.tfvars
@@ -1,6 +1,6 @@
 project     = "global-multi-tenant-saas-platform"
 environment = "prod"
-cidr_block = "10.10.0.0/16"
+cidr_block  = "10.10.0.0/16"
 
 
 enable_nat        = false
--- infra/terraform/modules/vpc/variables.tf
+++ infra/terraform/modules/vpc/variables.tf
@@ -17,8 +17,8 @@ variable "enable_nat" {
 
 # How many AZs to use (2 or 3)
 variable "az_count" {
-  type        = number
-  default     = 2
+  type    = number
+  default = 2
   validation {
     condition     = var.az_count >= 2 && var.az_count <= 3
     error_message = "az_count must be 2 or 3."
