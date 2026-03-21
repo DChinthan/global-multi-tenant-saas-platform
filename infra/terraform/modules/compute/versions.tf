fc6c56d9093c6481433fd4467f274306fc81622d
Chinthan Dinesh <chinthandp@gmail.com>
2026-03-20 20:02:43 -0400

    chore(terraform): add provider constraints for compute module

---
+++ infra/terraform/modules/compute/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.100"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.7"
+    }
+  }
+}
